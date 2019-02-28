b438bf8ba58f8cf4dc2178bae2dd2316cf6a6ad9
sglee <sg10.lee@gmail.com>
2019-02-28 18:13:25 +0900

    update thrift files

--- src/thrift/messenger.thrift
+++ src/thrift/messenger.thrift
@@ -5,33 +5,33 @@ namespace php thrift_gen
 struct ThriftMessage {
 	1:  required string 			_sender_id;
 	2:  optional string 			_receiver_id;
-	3:  required string			_timestamp;
+	3:  optional string			_timestamp;
 	4:  optional string 			_subject;
 	5:  optional i64 			_sequence_no;
 	6:  optional i64			_total_count;
 	7:  optional binary 			_binary;
 	8:  optional string 			_payload;
-	9:  optional list<ThriftMessage> 	_list_message;
-	10: optional set<ThriftMessage>		_set_message;
-	11: optional map<string,ThriftMessage> 	_map_message;
-	12: optional list<bool> 		_list_bool;
-	13: optional list<i16> 			_list_i16;
-	14: optional list<i32> 			_list_i32;
-	15: optional list<i64> 			_list_i64;
-	16: required list<double> 		_list_double;
-	17: optional list<string>		_list_string;
-	18: optional set<bool> 			_set_bool;
-	19: optional set<i16> 			_set_i16;
-	20: optional set<i32> 			_set_i32;
-	21: optional set<i64> 			_set_i64;
-	22: optional set<double> 		_set_double;
-	23: optional set<string>		_set_string;
-	24: optional map<string,bool> 		_map_bool;
-	25: optional map<string,i16> 		_map_i16;
-	26: optional map<string,i32> 		_map_i32;
-	27: optional map<string,i64> 		_map_i64;
-	28: optional map<string,double> 	_map_double;
-	29: optional map<string,string>		_map_string;
+	9:  optional list<bool> 		_list_bool;
+	10: optional list<i16> 			_list_i16;
+	11: optional list<i32> 			_list_i32;
+	12: optional list<i64> 			_list_i64;
+	13: optional list<double> 		_list_double;
+	14: optional list<string>		_list_string;
+	15: optional set<bool> 			_set_bool;
+	16: optional set<i16> 			_set_i16;
+	17: optional set<i32> 			_set_i32;
+	18: optional set<i64> 			_set_i64;
+	19: optional set<double> 		_set_double;
+	20: optional set<string>		_set_string;
+	21: optional map<string,bool> 		_map_bool;
+	22: optional map<string,i16> 		_map_i16;
+	23: optional map<string,i32> 		_map_i32;
+	24: optional map<string,i64> 		_map_i64;
+	25: optional map<string,double> 	_map_double;
+	26: optional map<string,string>		_map_string;
+	27:  optional list<ThriftMessage> 	_list_message;
+	28: optional set<ThriftMessage>		_set_message;
+	29: optional map<string,ThriftMessage> 	_map_message;
 }
 
 exception InvalidOperationException {
